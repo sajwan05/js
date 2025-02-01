5ee204d2a44bf4e3af7f2f7fb4fe7a8e7b3737bd
sajwan05 <surisajwan1998@gmail.com>
2025-02-01 13:35:57 +0530

    DATATYPES 7 primitives and 1 object data type

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all code as newer version of js
+
+// alert( 3 + 3); // we're using nodejs, not browser
+
+let name = "Suraj";
+let age = 26;
+let isLoggedIn = true;
+
+let state;
+let temp = null;
+
+/*console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);*/
+
+
+
+
+// Datatypes 
+/*
+// Primitives data types
+number
+bigInt
+string  '' "" ``
+boolean true/false
+null     stand alone value
+undefined not initialized
+symbol uniqueness
+
+// Non - primitives
+
+object -   key value pair
+*/ 
+
+console.log(typeof null);
+console.log(typeof undefined);
