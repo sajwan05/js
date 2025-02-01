1be424e37d782986fba3faa46d59fe281d64a112
sajwan05 <surisajwan1998@gmail.com>
2025-02-01 11:22:51 +0530

    VARIABLES AND LET VAR CONST played with variables why we should use let and const

---
+++ 01_basics/01-variables.js
@@ -0,0 +1,20 @@
+const accountId = 12455543;
+// console.log(accountEmail);
+let accountEmail = "surajsajwan@xyz.com"
+
+var accountPassword = "12345";
+accountCity = "Dehradun";
+
+let accountState;
+
+// console.log(accountPassword);
+// accountId = 456;  not allowed 
+
+/* do not use var-
+    Because it have issue with block and functional scope
+*/ 
+
+accountEmail = "surisaju@gmail.com";
+accountPassword = "121212";
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
