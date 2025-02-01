4e4cedaa5a5a269f774a408d66d23a826567cb6a
sajwan05 <surisajwan1998@gmail.com>
2025-02-01 16:17:39 +0530

    Conversion Played with implicit and explicit conversion and operators.

---
+++ 01_basics/03-conversion.js
@@ -0,0 +1,55 @@
+// let score = "fifty";
+
+// console.log(typeof score);
+// console.log(score);
+
+// let valueInNumber = Number(score);
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber);
+
+/*
+33 => 33
+"33abc" => NaN but NaN is basically of number type
+true => 1
+false => 0
+
+*/ 
+// hey 
+// let isLoggedIn = 78;
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// "suraj" => true; "" => false
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//**********************Operators*******************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// + - * / % 
+// console.log(2 + 2);
+// console.log( 2 - 2);
+// console.log( 2 * 2);
+// console.log( 2 ** 2);
+// console.log( 2 / 2);
+// console.log(2 % 2);
+
+let str1 = "hello, ";
+let str2 = "suraj";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// // console.log( 3 + 4 * 5 % 3);
+
+console.log(+true);
+console.log(+"");
