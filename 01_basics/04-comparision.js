6bf2b1007a6018b9a9c64bcd5c7c2d74ffc76d05
sajwan05 <surisajwan1998@gmail.com>
2025-02-01 16:48:34 +0530

    Comparision Learned some quirks of comparision and played with comparision operator

---
+++ 01_basics/04-comparision.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 === "2" );
+// console.log(  2 == "2");
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+
+// console.log("String: ")
+
+// console.log("2" > 1);
+// console.log("201" < 500);
+
+// console.log(null > 0);
+// console.log (null == 0);
+// console.log (null <= 0);
+// console.log(null >= 0);
+// console.log("Crazy: ");
+// console.log(null == undefined);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+
+
+
